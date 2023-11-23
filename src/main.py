042d09adc7ff954bcbe8f7c0d96401762a6a7d18
laflaretapee <103454307+laflaretapee@users.noreply.github.com>
2023-11-24 01:19:49 +0500

    Небольшой набросок бэка

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+
+from src.auth.base_config import auth_backend
+from src.auth.models import User
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
+from .operations.router import router as router_operations
+
+app = FastAPI(
+    title='APK'
+)
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(router_operations)
